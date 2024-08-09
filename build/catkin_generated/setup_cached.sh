0b11bf239af1296ac1a1d93c4fabc201489fbc65
Mark Adamik <m.adamik@vu.nl>
2024-08-09 13:53:10 +0200

    Resolved submodule conflict in orka

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/user/pel_ws/src/pel_ros/build/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/home/user/pel_ws/src/pel_ros/build/devel/lib:/home/user/pel_ws/devel/lib:/opt/ros/noetic/lib/x86_64-linux-gnu:/opt/ros/noetic/lib:/home/user/Downloads/Installers/webots-R2023b-x86-64/webots/lib/controller'
+export PATH='/opt/ros/noetic/bin:/home/user/.local/bin:/home/user/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PKG_CONFIG_PATH="/home/user/pel_ws/src/pel_ros/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/user/pel_ws/src/pel_ros/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/user/pel_ws/src/pel_ros/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/user/pel_ws/src/pel_ros:$ROS_PACKAGE_PATH"
---
+++ build/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest
+# Build directory: /home/user/pel_ws/src/pel_ros/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
